6a3804b4c5c5bc1a50680cdc11be495a03cbfc2d
NewMBW <115166381+Fatou-hub@users.noreply.github.com>
2026-05-07 10:16:09 +0200

    feat: add gitignore presence check

--- index.ts
+++ index.ts
@@ -1,3 +1,11 @@
+type GitHubFile = {
+    name: string;
+    path: string;
+    type: "file" | "dir";
+    url: string;
+    download_url: string | null;
+}
+
 const url =  process.argv[2];
 
 if(url === undefined){
@@ -12,9 +20,18 @@ const repo = parts[4];
 fetch(`https://api.github.com/repos/${owner}/${repo}`)
     .then((response)=> response.json())
     .then((data) => {
-    console.log("Response data:", data);
     if(data.visibility === "public"){
         console.log("This repository is public and accessible for the audit ! ");
+        fetch(`https://api.github.com/repos/${owner}/${repo}/contents`)
+            .then((res)=> res.json())
+            .then((dataLists :GitHubFile[])=>{
+                const gitIgnore = dataLists.find((element)=> element.name === ".gitignore");
+                if(gitIgnore){
+                    console.log(" GREAT !! This repository contains a gitignore file");
+                }else{
+                    console.log("CRITICAL ! This repository does not contains any gitignore !")
+                }
+            })
     }else{
         console.log("This repository is private ane not accessible for the audit !")
         process.exit(1);
